d36cc2872fe28a653c0b0c4c61fc5269532393e8
Hanzi Li <hanzili>
2023-11-02 19:54:36 -0400

    chore: remove unnecessary line

--- server/src/controllers/forum.controller.ts
+++ server/src/controllers/forum.controller.ts
@@ -1,17 +1,16 @@
 import ForumService from '../services/forum.service'
 import { Request, Response } from 'express'
-import { CustomRequest } from '../types/custom'
 
 class ForumController {
-    async getForum(req: Request, res: Response) {
-        try {
-            const forumId = req.params.forumId
-            const forum = await ForumService.getForum(forumId)
-            res.json(forum)
-        } catch (error) {
-            res.status(500).send(error)
-        }
+  async getForum(req: Request, res: Response) {
+    try {
+      const forumId = req.params.forumId
+      const forum = await ForumService.getForum(forumId)
+      res.json(forum)
+    } catch (error) {
+      res.status(500).send(error)
     }
+  }
 }
 
 export default new ForumController()
--- server/src/services/forum.service.ts
+++ server/src/services/forum.service.ts
@@ -1,11 +1,10 @@
-import { IForum } from "../models/Forum";
+import { IForum } from '../models/Forum'
 import ForumDao from '../dao/forum.dao'
-import { createObjectId } from '../utils/common.utils'
 
 class ForumService {
-    async getForum(forumId: string): Promise<IForum | null> {
-        return await ForumDao.findForumById(forumId)
-    }
+  async getForum(forumId: string): Promise<IForum | null> {
+    return await ForumDao.findForumById(forumId)
+  }
 }
 
 export default new ForumService()
